4fdf21af008d4f874056e6d93326bd889e793a13
Gary Anderson <gary@garandsoftware.co.uk>
2019-02-11 19:04:57 +0000

    Amended the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 const NotFound = () => (
   <div>
-    <h1>404 - Oh no&apos;s! We couldn&apos;t find that page :(</h1>
+    <h1>404 - Not Found</h1>
+    <h2>The page you have requested does not exist</h2>
   </div>
 )
 
